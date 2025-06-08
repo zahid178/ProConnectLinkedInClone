32aee2136333d784e91c8ee2beeb84e72f286d1f
zahid shaikh <shaikhzahid178@gmail.com>
2025-06-08 12:05:33 +0530

    Frontend Deployment

--- frontend/src/config/index.jsx
+++ frontend/src/config/index.jsx
@@ -2,7 +2,7 @@ import axios from "axios";
 
 import { configureStore } from "@reduxjs/toolkit";
 
-export const BASE_URL = "http://localhost:9080";
+export const BASE_URL = "https://proconnectlinkedinclone-q31n.onrender.com/";
 
 export const clientServer = axios.create({
     baseURL: BASE_URL,
